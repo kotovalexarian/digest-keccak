0217e6736c5498408c741d23fa3541f0d5a7525a
Alex Kotov <kotovalexarian@gmail.com>
2019-06-11 20:42:45 +0500

    Add test cases

--- test/test_all.rb
+++ test/test_all.rb
@@ -5,3 +5,4 @@ $LOAD_PATH.unshift(File.expand_path("ext"))
 require 'digest/keccak'
 require File.expand_path('test/test_usage')
 require File.expand_path('test/test_vectors')
+require File.expand_path('test/test_new')
