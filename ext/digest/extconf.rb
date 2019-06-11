61c2c18e6f122aba1b90dc1396f5bd53b31c4e4c
Alex Kotov <kotovalexarian@gmail.com>
2019-06-11 20:04:17 +0500

    Improve "ext/digest/extconf.rb"

--- ext/digest/extconf.rb
+++ ext/digest/extconf.rb
@@ -1,9 +1,26 @@
+#!/usr/bin/env ruby
 # frozen_string_literal: true
 
 require 'mkmf'
 
-have_header('ruby/digest.h')
-have_func('rb_str_set_len')
+def cflags(*args)
+  args.each do |str|
+    $CFLAGS += " #{str.shellescape} "
+  end
+end
 
-$CFLAGS << " -fvisibility=hidden"
-create_makefile('digest/keccak')
+def have_header!(*args)
+  exit 1 unless have_header(*args)
+end
+
+def have_func!(header, *args)
+  exit 1 unless have_func(*args, header)
+end
+
+cflags '-fvisibility=hidden'
+
+have_header! 'ruby/digest.h'
+
+have_func! 'rb_str_set_len'
+
+create_makefile 'digest/keccak' or exit 1
